a7dcb349410813bc18dff8b6a637ba846b8b62aa
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2023-04-14 03:26:36 +0530

    links exported to csv file

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-fs = require("fs");
-const arr = ['1', '2', '3']
+import fs from "fs";
+import puppeteer from "puppeteer";
 
-fs.appendFile("helloworld.csv", arr.join(','), function (err) {
-	if (err) return console.log(err);
-	console.log("Wrote Hello World in file helloworld.txt, just check it");
-});
+// const arr = ['1', '2', '3']
+
+// fs.appendFile("helloworld.csv", arr.join(','), function (err) {
+// 	if (err) return console.log(err);
+// 	console.log("Wrote Hello World in file helloworld.txt, just check it");
+// });
+
+//document.querySelectorAll('.row.align-center>div>div:nth-child(2)>div')
+let links_arr = [];
+
+const getPagesLink = async (url) => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+
+	await page.goto(url);
+
+	// Set screen size
+	await page.setViewport({ width: 1080, height: 1024 });
+
+	await page.waitForSelector(".row.align-center>div>div:nth-child(2)>div");
+	// const grid = await page.evaluate(() => {
+	// 	let arr = Array.from(
+	// 		document.querySelectorAll(".row.align-center>div>div:nth-child(2)>div")
+	// 	);
+	// })
+	const grid = await page.$$(".row.align-center>div>div:nth-child(2)>div");
+	for (const link of grid) {
+		let temp = await page.evaluate(
+			(el) =>
+				el.querySelectorAll("div .col-inner > a")?.[0].getAttribute("href"),
+			link
+		);
+		links_arr.push(temp);
+	}
+	console.log(links_arr);
+	fs.appendFile("links.csv", links_arr.join("\n"), function (err) {
+		if (err) return console.log(err);
+		console.log(
+			"*************************Links added to links.csv file*****************************"
+		);
+	});
+	await browser.close();
+};
+
+getPagesLink("https://animemotivation.com/category/quotes/page/3/");
