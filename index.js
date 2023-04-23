64e9e625092497ccbf974217f8d7caaf539f50c6
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2023-04-23 22:31:19 +0530

    :tada: structured like frostin repo

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ import fs from "fs";
 import puppeteer from "puppeteer";
 import chalk from "chalk";
 
-// const arr = ['1', '2', '3']
-
-// fs.appendFile("helloworld.csv", arr.join(','), function (err) {
-// 	if (err) return console.log(err);
-// 	console.log("Wrote Hello World in file helloworld.txt, just check it");
-// });
-
-//document.querySelectorAll('.row.align-center>div>div:nth-child(2)>div')
+let id = 8511;
 let links_arr = [];
 let links_copy = [];
+let browser;
+let page;
+let temp = [];
+let strObj = {};
+let str = "";
 
-const getPagesLink = async (url) => {
+// Function for getting the links of various anime quotes page and storing it in links.csv file.
+const getAMPagesLink = async (url) => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
@@ -35,7 +34,7 @@ const getPagesLink = async (url) => {
 				el.querySelectorAll("div .col-inner > a")?.[0].getAttribute("href"),
 			link
 		);
-		if (!temp.includes("porn" || !temp.includes('like-danmachi'))) {
+		if (!temp.includes("porn" || !temp.includes("like-danmachi"))) {
 			links_arr.push(temp);
 			links_copy.push(temp);
 		}
@@ -54,36 +53,51 @@ const getPagesLink = async (url) => {
 	await browser.close();
 };
 
-// getPagesLink("https://animemotivation.com/category/quotes/page/3/");
-
+// Maybe replacing this with the while loop would be better approach, breaking when page evaluates to null.
 for (let i = 1; i < 15; i++) {
-	await getPagesLink(`https://animemotivation.com/category/quotes/page/${i}/`);
+	await getAMPagesLink(`https://animemotivation.com/category/quotes/page/${i}/`);
 }
 
 for (let i = 0; i < links_copy.length; i++) {
 	try {
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
 		await page.goto(links_copy[i]);
 		await page.setViewport({ width: 1080, height: 1024 });
 		await page.waitForSelector("blockquote");
 		const blocks = await page.$$("blockquote");
-		let temp = [];
 		for (let i = 0; i < blocks.length; i++) {
-			temp.push(await page.evaluate((el) => el.innerText, blocks[i]));
+			str = await page.evaluate((el) => el?.innerText, blocks[i]);
+			if (str?.length) {
+				strObj = {
+					quote: str?.split(" – ")[0],
+					name: str?.split(" – ")[1],
+					id: id,
+				};
+				temp.push(strObj);
+				id++;
+			}
 		}
-		console.log(temp, temp.length);
-		fs.appendFile("quotes.csv", temp.join("\n"), function (err) {
-			if (err) return console.log(chalk.red("Error Occured--->", err));
-			console.log(
-				chalk.yellow("\n*************************************\n") +
-					chalk.bold.green("\nQuotes push to quotes.csv file\n") +
-					chalk.yellow("\n*************************************\n")
-			);
-		});
-		console.log('-----Closing browser-----')
-		await browser.close();
+		console.log(
+			chalk.blue("Pushed quotes to text file. ") +
+				chalk.underline.green("New number of quotes " + temp.length)
+		);
 	} catch (err) {
-		console.log(chalk.redBright(err) + ' occured at ' + chalk.green.bgCyan(i))
+		console.log(
+			chalk.redBright(err) +
+				" occured at link number -------> " +
+				chalk.green.bgCyan(i)
+		);
+		console.log(chalk.greenBright("Retrying the same link."));
 	}
+	await browser.close();
 }
+
+// console.log("Length---->", temp.length);
+fs.appendFile("quotes.text", JSON.stringify(temp, null, 2), function (err) {
+	if (err)
+		return console.log(
+			chalk.red("Error occured while writing to text file--->", err)
+		);
+	console.log(chalk.bold.green("Quotes push to quotes.text file\n"));
+});
